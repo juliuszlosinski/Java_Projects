ac4c124f36640d9afafbcab49af6dbd1de8d0378
Juliusz Losinski- PanFrog <72278818+juliuszlosinski@users.noreply.github.com>
2021-04-21 19:08:24 +0200

    Add files via upload

---
+++ Algorytmy_Ganetyczne/src/Tests/Tester.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Tester 
+{
+	public static void main(String args[])
+	{	
+		AG.Start_GA(120);
+	}
+}
