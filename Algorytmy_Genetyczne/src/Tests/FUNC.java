71fac12d744304491fec6b236c2ce1eb17cc4902
Juliusz Losinski- PanFrog <72278818+juliuszlosinski@users.noreply.github.com>
2021-04-28 14:43:33 +0200

    Add files via upload

---
+++ Algorytmy_Genetyczne/src/Tests/FUNC.java
@@ -0,0 +1,25 @@
+package Tests;
+
+
+public class FUNC //TODO: Funkcje do zadañ.
+{
+	// Funkcja do zadania 1
+	public static float Func_1(float x1, float x2)
+	{
+		//TODO: Funkcja do pierwszego algorytmu.
+		return (float) (-Math.pow(x1, 2)-Math.pow(x2, 2)+2);
+	}
+		
+	// Funkcja do zadania 2
+	static double Func_2(float[] vector, int n)
+	{
+		//TOD: Funkcja do drugiego algorytmu.
+		double res=0, A=10, N=A, pi=3.14;
+		for(int i=0;i<vector.length;i++)
+		{
+			res+=Math.pow(vector[i],2)-A*Math.cos(2*pi*vector[i]);
+		}
+		res+=A*n;
+		return res;
+	}
+}
---
+++ Algorytmy_Genetyczne/src/Tests/Tester.java
@@ -0,0 +1,10 @@
+package Tests;
+
+public class Tester //TODO: Klasa do testowania algorytmów.
+{
+	public static void main(String args[])
+	{	
+		//TODO: Rozpoczecie wykonywania alogrytmu dla okreœlonej jako parametr formalny wielkoœci populacji.
+		AG.Start_GA(140);
+	}
+}
